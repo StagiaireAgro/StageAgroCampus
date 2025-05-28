2ebacafc4ea42c48f73d0706ff8c518d9bb04697
StagiaireAgro <stageagrocampus@gmail.com>
2025-05-28 12:49:21 +0200

    Update fonction.R

--- Pricco/Regroupement des modalitées/Separation_fichiers_inouss/fonction.R
+++ Pricco/Regroupement des modalitées/Separation_fichiers_inouss/fonction.R
@@ -1,8 +1,12 @@
+# OUEDRAOGO Teeg-wendé Inoussa
+# twinoussa55@gmail.com
+# -------------------------------------------------------------------------
 
 library(yaml)
 library(vroom)
 library(stringr)
 library(stringi)
+
 
 clean <- function(variable){
   x <- str_to_lower(variable)
@@ -17,9 +21,10 @@ transform_in_regex <- function(mot) {
   return(paste0(mots_regex, collapse = ""))
 }
 
+
 search_product <- function(one_prod, ls_prod_ordered, ls_prod_regex){
   prod_find <- c()
-  prod_tmp <- one_prod
+  prod_tmp <- paste(unique(unlist(strsplit(one_prod, " "))), collapse = " ")
   for(indice in seq_along(ls_prod_regex)){
     pattern <- ls_prod_regex[indice]
     if(str_detect(prod_tmp, regex(pattern, ignore_case = TRUE))){
@@ -35,7 +40,7 @@ search_product <- function(one_prod, ls_prod_ordered, ls_prod_regex){
 produits <- read_yaml("products.yaml")
 
 # Socleo datasets ---------------------------------------------------------
-path = "data/socleo"
+path = "Data/Résultats données triées/socleo"
 socleo_contents <- list.files(path, full.names = TRUE)
 
 socleo_2021 <- vroom(socleo_contents[1])
@@ -43,46 +48,86 @@ socleo_2022 <- vroom(socleo_contents[2])
 socleo_2023 <- vroom(socleo_contents[3])
 socleo_2024 <- vroom(socleo_contents[4])
 
+
+
+
+
+
+
 split_data <- function(socleo_2021, produits){
   liste_par_fruit <- list()
   # Nettoyage de la liste des mots produits
   clean_produits <- clean(produits) # miniscule, gestion accents & carac spéciaux
-  clean_produits_sans_espaces <- gsub(" ", "|", clean_produits) # Supp espace
+  clean_produits_sans_espaces <- gsub(" ", "", clean_produits) # Supp espace
   # Classer le regex et les mots de la liste en decroissant en fonction du vecteur retiré d'espace
-  oder_clean_produits_sans_espaces <- order(nchar(clean_produits_sans_espaces), decreasing = TRUE) # Du plus long au plus court mot
-  produits_regex <- sapply(clean_produits, transform_in_regex) # creation des regex de la liste des produits
-  produits_regex <- produits_regex[oder_clean_produits_sans_espaces] # Ordonner de manière decroissante le regex
-  clean_produits <- clean_produits[oder_clean_produits_sans_espaces] # Ordonner de manière decroissante la liste des produits
-  
-  socleo_2021[["clean_name"]] <- as.factor(socleo_2021[["name"]]) # Transformer la colonne name en facteur
-  levels(socleo_2021[["clean_name"]]) <- clean(levels(socleo_2021[["clean_name"]])) # Nettoyage des levels 
-  levels(socleo_2021[["clean_name"]]) <- gsub("\\s+", "", levels(socleo_2021[["clean_name"]])) # Supprimer les espaces des levels
-  
-  # Filtre de dataset (eligible, non eligible)
-  levels_pas_ok <- levels(socleo_2021[["clean_name"]])[!str_detect(levels(socleo_2021[["clean_name"]]), paste(clean_produits, collapse = "|"))]
-  socleo_2021_pas_ok <- socleo_2021[socleo_2021[["clean_name"]] %in% levels_pas_ok,]
-  socleo_2021_ok <- socleo_2021[!socleo_2021[["clean_name"]] %in% levels_pas_ok,]
-  
-  # 
-  levels_socleo_2021_ok <- levels(socleo_2021_ok[["clean_name"]])
+  oder_clean_produits_sans_espaces <- order(nchar(clean_produits_sans_espaces), decreasing = TRUE) # Ordre
+  # creation des regex de la liste des produits
+  produits_regex <- sapply(clean_produits, transform_in_regex)
+  # Ordonner de manière decroissante le regex
+  produits_regex <- produits_regex[oder_clean_produits_sans_espaces]
+  # Ordonner de manière decroissante la liste des produits
+  clean_produits <- clean_produits[oder_clean_produits_sans_espaces]
+  # Transformer la colonne name en facteur
+  socleo_2021[["clean_name"]] <- as.factor(socleo_2021[["name"]])
+  # Nettoyage des levels 
+  levels(socleo_2021[["clean_name"]]) <- clean(levels(socleo_2021[["clean_name"]]))
+  # Supprimer les espaces des levels
+  levels(socleo_2021[["clean_name"]]) <- gsub("\\s+", "", levels(socleo_2021[["clean_name"]])) 
+  levels_socleo_2021_ok <- levels(socleo_2021[["clean_name"]])
   classification <- sapply(levels_socleo_2021_ok, search_product, 
-                            ls_prod_ordered = clean_produits, 
-                            ls_prod_regex = produits_regex)
-  socleo_2021_ok[["classif"]] <- classification[as.character(socleo_2021_ok[["clean_name"]])]
+                           ls_prod_ordered = clean_produits, 
+                           ls_prod_regex = produits_regex)
+  socleo_2021[["classif"]] <- classification[as.character(socleo_2021[["clean_name"]])]
+  socleo_2021[["clean_name"]] <- clean(as.factor(socleo_2021[["name"]]))
+  socleo_2021[["classif"]][!is.na(socleo_2021[["classif"]]) & socleo_2021[["classif"]] == ""] <- NA
   
+  
+  # Dataset autre & propre
+  data_ok <- socleo_2021[!is.na(socleo_2021[["classif"]]) & str_count(socleo_2021[["classif"]], ",") == 0, ]  
+  data_pas_ok <- socleo_2021[is.na(socleo_2021[["classif"]]) | str_count(socleo_2021[["classif"]], ",") >= 1, ]
   
   for(fruit in clean_produits) {
     liste_par_fruit[[paste0("data_",fruit, sep="")]] <- 
-      socleo_2021_ok[socleo_2021_ok[["classif"]] == fruit, ]
+      data_ok[data_ok[["classif"]] == fruit, ]
   }
   
-  socleo_2021_pas_ok$classif <- NA
   
-  liste_par_fruit[["data_autre"]] <- rbind(
-    socleo_2021_pas_ok, socleo_2021_ok[str_count(socleo_2021_ok[["classif"]], ",") >= 1, ])
+  # Filtrer à nouveau data_pas_ok
+  new_filter <- data_pas_ok[!is.na(data_pas_ok[["classif"]]),]
+  
+  levels_new_filter <- levels(as.factor(new_filter[["clean_name"]]))
+  new_classification <- sapply(levels_new_filter, search_product, 
+                          ls_prod_ordered = clean_produits, 
+                          ls_prod_regex = produits_regex)
+  
+  new_filter[["classif"]] <- new_classification[new_filter[["clean_name"]]]
+  levels_new_filter_prod <-  new_filter[str_count(new_filter[["classif"]], ",") == 0, "classif"]
+  levels_new_filter_prod <- levels(as.factor(levels_new_filter_prod[["classif"]]))
+  for(un_nom_produit in levels_new_filter_prod){
+    liste_nom <- paste0("data_",un_nom_produit, sep="")
+    new <- new_filter[new_filter[["classif"]]==un_nom_produit,]
+    liste_par_fruit[[paste0("data_",un_nom_produit, sep="")]] <- rbind(
+      liste_par_fruit[[paste0("data_",un_nom_produit, sep="")]],new)
+    lignes_a_supprimer <- which(new_filter[["classif"]] == un_nom_produit)
+    new_filter <- new_filter[-lignes_a_supprimer, ]
+  }
+  
+  liste_par_fruit[["data_autre"]] <- rbind(new_filter, data_pas_ok[is.na(data_pas_ok[["classif"]]),])
   
   return(liste_par_fruit)
 }
 
-s <- split_data(socleo_2021, produits)
-ls(s)
+test <- split_data(socleo_2021, produits)
+
+
+
+View(test)
+View(test$data_autre)
+View(test$data_boeuf)
+sum(sapply(test, function(x) nrow(x)))
+
+
+s <- test$data_autre
+
+ 
+
